0f7d453776528c9cd175d17710b10cb139f0c459
Ira <kalmykova_ira@ukr.net>
2019-06-14 21:57:35 +0300

    Updates

---
+++ precache-manifest.ade7c4eebf92664f6d54ef160fd84541.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de3e0e464df3ed071f5fde8a1884bfcc",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "8cacb63128c22c93ccb1",
+    "url": "/todo-app/static/css/main.9cc93ff6.chunk.css"
+  },
+  {
+    "revision": "5694e1898ca4c92fd14d",
+    "url": "/todo-app/static/js/2.6408f43b.chunk.js"
+  },
+  {
+    "revision": "8cacb63128c22c93ccb1",
+    "url": "/todo-app/static/js/main.e6d76946.chunk.js"
+  },
+  {
+    "revision": "f7728c30f6339eef41ed",
+    "url": "/todo-app/static/js/runtime~main.eca9ff85.js"
+  }
+]);
